402f13193bb342cc349058d70a3aaaa87c752aad
shubham <shubham22121@gmail.com>
2023-02-08 04:22:05 +0530

    unary and bidirectional streaming grpc

---
+++ client/main.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"log"
+
+	pb "github.com/shubham/go-grpc/proto"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
+)
+
+const (
+	port = "8080"
+)
+
+func main() {
+	conn, err := grpc.Dial("localhost:"+port, grpc.WithTransportCredentials(insecure.NewCredentials()))
+	if err != nil {
+		log.Fatalf("Did not connect: %v", err)
+	}
+	defer conn.Close()
+
+	client := pb.NewHelloServiceClient(conn)
+
+	// names := &pb.NamesList{
+	// 	Names: []string{"Akhil", "Alice", "Bob"},
+	// }
+
+	callSayHello(client)
+	//callSayHelloServerStream(client, names)
+	//callSayHelloClientStream(client, names)
+	// callSayHelloBidirectionalStream(client, names)
+
+}
